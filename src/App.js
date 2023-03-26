a11f0a042a3348363780ce09dd58e3a9fe7a4a2a
M-Hassan-Raza <h.raza1164@yahoo.com>
2023-03-26 19:27:59 +0500

    Added State to Text field

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import React, {useState} from 'react';
 import Form from './components/Form'
 import TodoList from './components/TodoList'
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header>
         <h1>Rai's TODO List</h1>
       </header>
-      <Form />
+      <Form setInputText = {setInputText}/>
       <TodoList />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-function Form() {
+function Form(props) {
+    const inputTextHandler = (e) => {
+        props.setInputText(e.target.value);
+    }
     return (
         <form>
-            <input type="text" className="todo-input" />
+            <input onChange={inputTextHandler} type="text" className="todo-input" />
             <button className="todo-button" type="submit">
                 <i className="fas fa-check"></i>
             </button>
