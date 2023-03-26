ba65374b2cbaf5a3ce3648296062c73b840763a2
M-Hassan-Raza <h.raza1164@yahoo.com>
2023-03-26 20:22:13 +0500

    Added priority, added states, added event handlers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form'
 import TodoList from './components/TodoList'
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [todosList, setTodosList] = useState([]);
+  const [priority, setPriority] = useState("");
   return (
     <div className="App">
       <header>
         <h1>Rai's TODO List</h1>
       </header>
-      <Form setInputText = {setInputText}/>
+      <Form
+        todosList={todosList}
+        setTodosList={setTodosList}
+        setInputText={setInputText}
+        inputText={inputText}
+        priority={priority}
+        setPriority={setPriority}
+      />
       <TodoList />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,22 @@ function Form(props) {
     const inputTextHandler = (e) => {
         props.setInputText(e.target.value);
     }
+    const inputPriorityHandler = (e) => {
+        props.setPriority(e.target.value);
+    }
+    const submitHandler = (e) => {
+        e.preventDefault();
+        props.setTodosList([
+            ...props.todosList,
+            { text: props.inputText, completed: false, id: Math.random() * 1000, priority: 0 }
+        ]);
+        props.setInputText("");
+    };
     return (
         <form>
-            <input onChange={inputTextHandler} type="text" className="todo-input" />
-            <button className="todo-button" type="submit">
+            <input onChange={inputTextHandler} type="text" className="todo-input" placeholder="TODO Item"/>
+            <input onChange={inputPriorityHandler} type="number" min="1" max="5" className="todo-input priority" placeholder="Priority"/>
+            <button onClick={submitHandler} className="todo-button" type="submit">
                 <i className="fas fa-check"></i>
             </button>
             <div className="select">
