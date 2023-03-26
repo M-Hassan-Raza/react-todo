4be75b471f9e9037f58de7fd87b05e7549120ec2
M-Hassan-Raza <h.raza1164@yahoo.com>
2023-03-26 17:22:04 +0500

    Set up basic Project, added componenets

---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoList() {
+    return (
+        <div class="todo-container">
+            <ul class="todo-list"></ul>
+        </div>
+    );
+}
+
+export default TodoList;
