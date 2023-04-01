6a4b40abdac86a6095211e0781074e8674ae3d9c
M-Hassan-Raza <h.raza1164@yahoo.com>
2023-04-01 21:02:57 +0500

    Updated State for InputText

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,24 +11,18 @@ function Form(props) {
         e.preventDefault();
         props.setTodosList([
             ...props.todosList,
-            { text: props.inputText, completed: false, id: Math.random() * 1000, priority: 0 }
+            { text: props.inputText, completed: false, id: parseInt(Math.random() * 1000), priority: props.priority }
         ]);
         props.setInputText("");
     };
     return (
         <form>
-            <input onChange={inputTextHandler} type="text" className="todo-input" placeholder="What is the task today?"/>
-            <input onChange={inputPriorityHandler} type="number" min="1" max="5" className="todo-input priority" placeholder="How important it is?"/>
+            <input onChange={inputTextHandler} value={props.inputText} type="text" className="todo-input" placeholder="What is the task today?"/>
+            <input onChange={inputPriorityHandler} type="number" min="1" max="5" className="todo-input priority" placeholder="How important it is? (1-5)"/>
             <button onClick={submitHandler} className="todo-button" type="submit">
                 <i className="fas fa-check"></i>
             </button>
-            <div className="select">
-                <select name="todos" className="filter-todo">
-                    <option value="all">All</option>
-                    <option value="completed">Completed</option>
-                    <option value="uncompleted">Uncompleted</option>
-                </select>
-            </div>
+            
         </form>
     );
 }
